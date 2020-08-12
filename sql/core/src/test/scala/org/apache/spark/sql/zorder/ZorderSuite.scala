d809a3966a3084b6992a4213139fd51f4c2f2e83
Karuppayya Rajendran <karuppayya1990@gmail.com>
2020-08-12 11:51:40 -0700

    Refactor

--- sql/core/src/test/scala/org/apache/spark/sql/zorder/ZorderSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/zorder/ZorderSuite.scala
@@ -18,46 +18,17 @@
 package org.apache.spark.sql.zorder
 
 import java.time.{LocalDate, Month}
-import java.util
-import java.util.Comparator
 import java.util.concurrent.TimeUnit.NANOSECONDS
 
-import org.apache.spark.sql.internal.SQLConf
-import org.apache.spark.sql.test.SQLTestData.TestData2
-import org.apache.spark.sql.test.{SQLTestUtils, SharedSparkSession}
 import org.apache.spark.sql.QueryTest
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.test.{SharedSparkSession, SQLTestUtils}
+import org.apache.spark.sql.test.SQLTestData.TestData2
 
 case class Data(intCol: Long, longCol1: Long,
                 longCol2: Long = 0, time: Long = 0L, random: Int = 0)
 case class Metric(numRecords: Long, isZordered: Boolean, orderedColCount: Int,
                   timeTakenInMs: Long, pred: String, predCount: Int)
-
-class ZorderComparator[T] extends Comparator[T] with Serializable {
-
-  def compare(o11: T, o22: T): Int = {
-    val o1 = o11.asInstanceOf[Data]
-    val o2 = o22.asInstanceOf[Data]
-    val values1: Array[Long] = Array(o1.intCol, o1.longCol1, o1.longCol2)
-    val values2: Array[Long] = Array(o2.intCol, o2.longCol1, o2.longCol2)
-    var index = 0
-    while (index < values1.length - 1 && less_msb(values1(index) ^ values2(index),
-      values1(index + 1) ^ values2(index + 1))) {
-      index = index + 1
-    }
-    if ((values1(index) - values2(index)) == 0) {
-      0
-    } else if ((values1(index) - values2(index)) > 0) {
-      1
-    } else {
-      -1
-    }
-  }
-
-  def less_msb(x: Long, y: Long): Boolean = {
-    x < y && x < (x ^ y)
-  }
-
-}
 
 class ZorderSuite extends QueryTest
   with SharedSparkSession with SQLTestUtils {
@@ -67,57 +38,14 @@ class ZorderSuite extends QueryTest
     loadTestData()
   }
 
-  test("Range partitioning1") {
-    val arr = (1 to 10).flatMap {
-      x =>
-        (1 to 10).flatMap {
-          y =>
-            (1 to 10).map {
-              z => Data(x, y, z)
-            }
-        }
-    }
-    util.Arrays.sort(arr.toArray, new ZorderComparator[Data])
-  }
-
-  test("Range partitioning") {
-    val loc = "file:///tmp/zorder/5"
-    val df = sql("select * from testdata2 order by a, b")
-    df.repartition(1)
-      .write
-      .mode("overwrite")
-      .csv(loc)
-  }
-
-  test("z-order - Golden - Naive") {
-
-    import testImplicits._
-
-
-    val data = (0 to 7).flatMap {
-      x =>
-        (0 to 7).map {
-          y => Data(x, y)
-        }
-    }
-    val dsRdd = data.toDS()
-      .repartition(1)
-      .toJavaRDD.zipWithIndex()
-      .sortByKey(new ZorderComparator[Data])
-      .keys()
-    val newDS = spark.createDataFrame(dsRdd).as[Data]
-    val results = newDS.collect()
-    results
-  }
-
-  test("without z order") {
+  test("Z order - init") {
     def less_msb(x: Long, y: Long): Boolean = {
       x < y && x < (x ^ y)
     }
 
-    val data = (0 to 7).flatMap {
+    val data = (0 to 6).flatMap {
       x =>
-        (0 to 7).map {
+        (0 to 6).map {
           y => TestData2(x, y)
         }
     }
@@ -126,16 +54,22 @@ class ZorderSuite extends QueryTest
       case (data1, data2) =>
         val values1: Array[Long] = Array(data1.a, data1.b)
         val values2: Array[Long] = Array(data2.a, data2.b)
-        var index = 0
-        while (index < values1.length - 1 && less_msb(values1(index) ^ values2(index),
-          values1(index + 1) ^ values2(index + 1))) {
-          index = index + 1
+        var msd = 0
+        for (dim <- 1 until values1.length) {
+          val l1 = values1(msd) ^ values2(msd)
+          val l2 = values1(dim) ^ values2(dim)
+          if (less_msb(l1, l2)) {
+            msd = dim;
+          }
         }
-        (values1(index) - values2(index)) < 0
+        values1(msd) < values2(msd)
     }
 
     import testImplicits._
-    sorted.toDS().repartition(1).write.csv("/tmp/golden")
+    val results = data.toDS()
+      .repartition(1).orderBy("a", "b").collect()
+    assert(sorted == golden)
+    assert(results.toSeq == golden)
   }
 
   test("z-order - Actual") {
@@ -192,11 +126,11 @@ class ZorderSuite extends QueryTest
                 if (zorder) {
                   df.orderBy(cols.head, cols.drop(1): _*)
                     .write.mode("overwrite").parquet(s"$basePath/$num/zorder/$key")
-                  println(s"Write succesful record count = $num loc: ${s"$basePath/$num/zorder/$key"}")
+                  // println(s"Write succesful record count = $num loc: ${s"$basePath/$num/zorder/$key"}")
                 } else {
                   df.orderBy("random")
                     .write.mode("overwrite").parquet(s"$basePath/$num/orig/$key")
-                  println(s"Write succesful record count = $num loc: ${s"$basePath/$num/orig/$key"}")
+                  // println(s"Write succesful record count = $num loc: ${s"$basePath/$num/orig/$key"}")
                 }
             }
         }
@@ -230,6 +164,58 @@ class ZorderSuite extends QueryTest
       .orderBy("numRecords", "predCount", "orderedColCount")
 
     newDF.show(100)
+  }
+
+  val golden = {
+    Seq(TestData2(0, 0),
+      TestData2(0, 1),
+      TestData2(1, 0),
+      TestData2(1, 1),
+      TestData2(0, 2),
+      TestData2(0, 3),
+      TestData2(1, 2),
+      TestData2(1, 3),
+      TestData2(2, 0),
+      TestData2(2, 1),
+      TestData2(3, 0),
+      TestData2(3, 1),
+      TestData2(2, 2),
+      TestData2(2, 3),
+      TestData2(3, 2),
+      TestData2(3, 3),
+      TestData2(0, 4),
+      TestData2(0, 5),
+      TestData2(1, 4),
+      TestData2(1, 5),
+      TestData2(0, 6),
+      TestData2(1, 6),
+      TestData2(2, 4),
+      TestData2(2, 5),
+      TestData2(3, 4),
+      TestData2(3, 5),
+      TestData2(2, 6),
+      TestData2(3, 6),
+      TestData2(4, 0),
+      TestData2(4, 1),
+      TestData2(5, 0),
+      TestData2(5, 1),
+      TestData2(4, 2),
+      TestData2(4, 3),
+      TestData2(5, 2),
+      TestData2(5, 3),
+      TestData2(6, 0),
+      TestData2(6, 1),
+      TestData2(6, 2),
+      TestData2(6, 3),
+      TestData2(4, 4),
+      TestData2(4, 5),
+      TestData2(5, 4),
+      TestData2(5, 5),
+      TestData2(4, 6),
+      TestData2(5, 6),
+      TestData2(6, 4),
+      TestData2(6, 5),
+      TestData2(6, 6))
   }
 }
 
