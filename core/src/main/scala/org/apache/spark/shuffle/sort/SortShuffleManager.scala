fef74408f43362b4ac1ed587415f125fe4e1637f
Karuppayya Rajendran <karuppayya.rajendran@salesforce.com>
2025-09-20 18:32:21 -0700

    Shufflevault: Shuffle on S3

--- core/src/main/scala/org/apache/spark/shuffle/sort/SortShuffleManager.scala
+++ core/src/main/scala/org/apache/spark/shuffle/sort/SortShuffleManager.scala
@@ -24,6 +24,7 @@ import scala.jdk.CollectionConverters._
 import org.apache.spark._
 import org.apache.spark.internal.Logging
 import org.apache.spark.shuffle._
+import org.apache.spark.shuffle.ShuffleDependencyRegistry
 import org.apache.spark.shuffle.api.ShuffleExecutorComponents
 import org.apache.spark.util.collection.OpenHashSet
 
@@ -184,6 +185,8 @@ private[spark] class SortShuffleManager(conf: SparkConf) extends ShuffleManager 
         }
       }
     }
+    // Also unregister from ShuffleDataRegistry
+    ShuffleDependencyRegistry.unregisterShuffleDependency(shuffleId)
     true
   }
 
